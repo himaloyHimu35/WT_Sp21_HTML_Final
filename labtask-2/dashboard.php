14afaae59b7bd3376dccc6e513cda0ca9fdf6c69
himaloyHimu35 <17-35042-2@student.aiub.edu>
2021-03-28 21:56:08 +0600

    "dashboard"

---
+++ labtask-2/dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body >
+<div style="text-align: center; margin-top: 15%; border-style: solid; background-color: lightblue">
+	 <a href="Allstudents.php"  target="_blank">All Student</a> <br> <br>
+	 <a href="addstudents.php " target="_blank">ADD Student</a> <br> <br>
+	 <a href="editstudents.php" target="_blank">Edit Student</a> <br> <br>
+	 <a href="#">More</a> 
+</div>
+</body>
+</html>
