c301e9df4a77c1f78574bfe717979baaa1272781
himaloyHimu35 <17-35042-2@student.aiub.edu>
2021-04-05 14:11:43 +0600

    "Add catagory"

---
+++ labtask-3/addcategory.php
@@ -0,0 +1,18 @@
+<?php include 'adminheader.php';?>
+<!--addproduct starts -->
+	<div class="center">
+		<form class="form-horizontal form-material">
+			<div class="form-group">
+				<h4 class="text">Name:</h4> 
+				<input type="text" class="form-control">
+			</div>
+			
+			<div class="form-group text-center">
+				
+				<input type="submit" class="btn btn-success" value="Add Category" class="form-control">
+			</div>
+		</form>
+	</div>
+
+<!--addproduct ends -->
+<?php include 'adminfooter.php';?>
